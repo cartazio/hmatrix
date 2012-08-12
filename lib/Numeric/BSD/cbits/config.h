2fca7ca8a1ce8ee43192c4bcf700c8798f5c5439
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2012-08-12 18:01:47 -0400

    adding some substitute bindings via blas?

---
+++ lib/Numeric/BSD/cbits/config.h
@@ -0,0 +1,33 @@
+
+/* cbits/config.h.  Generated from config.h.in by configure.  */
+/* cbits/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to a macro mangling the given Fortan function name */
+#define F77_FUNC(name) name ## _
+
+/* Define if you have a BLAS library. */
+#define HAVE_BLAS 1
+
+/* Define if you have LAPACK library. */
+#define HAVE_LAPACK 1
+
+/* Define to 1 if you have the `m' library (-lm). */
+#define HAVE_LIBM 1
+
+/* Define to the address where bug reports for this package should be sent. */
+// #define PACKAGE_BUGREPORT "patperry@gmail.com"
+
+/* Define to the full name of this package. */
+// #define PACKAGE_NAME "Haskell Linear Algebra"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "Haskell Linear Algebra 0.8"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "haskell-linear-algebra"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.8"
